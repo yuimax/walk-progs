43f42fded6bd3a77f1928d2f866ec7a13d4c1607
yuimax <vimalakitty@yuimax.org>
2026-08-29 11:53:53 +0900

    cmt 2026/08/29 11:53 (UTC+9)

--- 001-MainWindow/001-MainWindow.go
+++ 001-MainWindow/001-MainWindow.go
@@ -2,30 +2,29 @@
 package main
 
 import (
-    "github.com/lxn/walk"
+	"github.com/lxn/walk"
 )
 
 func main() {
-    // メインウィンドウの作成
-    mw, err := walk.NewMainWindow()
-    if err != nil {
-        panic(err)
-    }
-    defer mw.Dispose()
+	// メインウィンドウの作成
+	mw, err := walk.NewMainWindow()
+	if err != nil {
+		panic(err)
+	}
 	
-    // メインウィンドウの設定
-    mw.SetTitle("001: walk.MainWindow")
-    mw.SetSize(walk.Size{600, 400})
+	// メインウィンドウの設定
+	mw.SetTitle("001: walk.MainWindow")
+	mw.SetSize(walk.Size{600, 400})
 	mw.SetMinMaxSize(
 		walk.Size{300, 200}, // MinSize 
 		walk.Size{800, 600}, // MaxSize
 	)
 
-    // レイアウト設定
-    layout := walk.NewVBoxLayout()
-    mw.SetLayout(layout)
+	// レイアウト設定
+	layout := walk.NewVBoxLayout()
+	mw.SetLayout(layout)
 	
-    // ウィンドウを表示してイベントループを開始
-    mw.Show()
-    mw.Run()
+	// ウィンドウを表示してイベントループを開始
+	mw.Show()
+	mw.Run()
 }
